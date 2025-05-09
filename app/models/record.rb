e269959e06950bd25c224e53abb8eeb44329241b
amnindersingh12 <amnindersingh58611@gmail.com>
2025-05-09 05:26:35 +0000

    Make photo mandatory when creating a record

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,7 +2,7 @@ class Record < ApplicationRecord
   belongs_to :user
   has_many :attendances, dependent: :destroy
   has_one_attached :photo
-  validates :name, presence: true
+  validates :name, :photo, presence: true
 
 
   def self.ransackable_attributes(auth_object = nil)
@@ -17,4 +17,3 @@ class Record < ApplicationRecord
     end
     end
 end
-# https://grok.com/share/c2hhcmQtMg%3D%3D_c452595e-824e-4b77-af5b-e04e5e1dfa0e
